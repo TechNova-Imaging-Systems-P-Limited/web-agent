5617fb8ad877492f9634d5d0fdb7e79f8a7b8194
Rishab <rishab.singh@technovaindia.com>
2026-02-25 10:26:32 +0530

    api_key

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-AZURE_OPENAI_API_KEY = os.environ.get("AZURE_OPENAI_API_KEY", "Replace_With_Your_Azure_API_Key")
+AZURE_OPENAI_API_KEY = os.environ.get("AZURE_OPENAI_API_KEY", "Azure_API_Key")
 AZURE_OPENAI_ENDPOINT = os.environ.get("AZURE_OPENAI_ENDPOINT", "https://openai-ragbot.openai.azure.com/")
 AZURE_OPENAI_API_VERSION = os.environ.get("AZURE_OPENAI_API_VERSION", "2024-12-01-preview")
 
 AZURE_DEPLOYMENT_NAME = os.environ.get("AZURE_DEPLOYMENT_NAME", "gpt-4.1-mini")
 AZURE_EMBEDDING_DEPLOYMENT_NAME = os.environ.get("AZURE_EMBEDDING_DEPLOYMENT_NAME", "text-embedding-3-small")
 
-SERPER_API_KEY = os.environ.get("SERPER_API_KEY", "Replace_With_Your_Serper_API_Key")
+SERPER_API_KEY = os.environ.get("SERPER_API_KEY", "Serper_API_Key")
